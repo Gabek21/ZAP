6a5a572f2fcfdcc64332a924607942cdae779ef8
Pawel Ankersztajn <gabek@vp.pl>
2011-11-15 19:36:58 +0100

    ...

---
+++ Zad1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, wynik;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    wynik = (a+b+c)/3
+    cout << wynik << endl;
+    return 0;
+}
